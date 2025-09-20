39f6054d89d8e1ef98844fd3ea7c754ef86ef22a
kirill24050411 <kirill24050411@gmail.com>
2025-09-20 19:01:55 +0400

    tgu

--- top_gpt_ultra/wsgi.py
+++ top_gpt_ultra/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'top_gpt_ultra.top_gpt_ultra.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'top_gpt_ultra.settings')
 
 application = get_wsgi_application()
