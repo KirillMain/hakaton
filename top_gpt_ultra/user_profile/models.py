b9e59a7db9ee50467b9e8d229c2900b7dfbb33e6
Данил <danil.89376467241@gmail.com>
2025-09-20 09:36:59 +0400

    user profile model

--- top_gpt_ultra/user_profile/models.py
+++ top_gpt_ultra/user_profile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    inn = models.CharField(max_length=12, blank=True, null=True)
+    slice_date = models.DateField(
+        null=True,
+    )
+    settings = models.JSONField(
+        verbose_name="Настройки пользователя", default=dict, blank=True
+    )
