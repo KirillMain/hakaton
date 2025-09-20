a61c9d514638cec4d204743b0f3cec579215fa8a
oum security <kirill24050411@gmail.com>
2025-09-20 14:41:01 +0400

    fixes + gitignore

--- top_gpt_ultra/top_gpt_ultra/urls.py
+++ top_gpt_ultra/top_gpt_ultra/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/v1/profile", include("user_profile.urls")),
-    path("api/v1/assistant/parse",include("assistant.urls")),
+    path("api/v1/profile/", include("user_profile.urls")),
+    path("api/v1/assistant/",include("assistant.urls")),
 ]
