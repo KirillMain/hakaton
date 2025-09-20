053134df6f8865945efe6f302c4a5bc311844a0f
kirill24050411 <kirill24050411@gmail.com>
2025-09-20 19:12:34 +0400

    qwe

--- top_gpt_ultra/urls.py
+++ top_gpt_ultra/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from top_gpt_ultra.views import Alive
 
 urlpatterns = [
+    path("alive/", Alive.as_view()),
     path("api/v1/profile/", include("user_profile.urls")),
     path("api/v1/assistant/",include("assistant.urls")),
 ]
