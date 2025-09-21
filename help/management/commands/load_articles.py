feb0732419a3e7b6b4fb0dc87843b722c7b6f744
shottaneflow <danil.89376467241@gmail.com>
2025-09-21 11:42:57 +0400

    1

--- help/management/commands/load_articles.py
+++ help/management/commands/load_articles.py
@@ -1,6 +1,6 @@
 import json
 from django.core.management.base import BaseCommand
-from help.models import Article
+from help.models import ArticleNew
 from django.core.exceptions import MultipleObjectsReturned
 import re
 
@@ -18,21 +18,19 @@ class Command(BaseCommand):
             for service in category.get('articlesByService', []):
                 for article in service.get('articles', []):
                     simplified_article = {
+                    "id": article.get("id",""),
                     "largeName": article.get('largeName', ''),
                     "url": f"https://zakupki.mos.ru/knowledgebase/article/details/ais/{article.get('id', '')}"
                     }
                     simplified_data.append(simplified_article)
-        print(simplified_data)
-
-   
-        
 
         for item_data in simplified_data:
             try:
-                Article.objects.get_or_create(
+                ArticleNew.objects.get_or_create(
                     url=item_data.get("url"),  
                     defaults={
-                        'title': item_data.get("largeName")  
+                        'title': item_data.get("largeName"),
+                        'id': item_data.get("id")
                     }
                 )
             except :
--- help/models.py
+++ help/models.py
@@ -4,9 +4,17 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class Article(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     url = models.URLField(verbose_name="URL статьи", unique=True)
+
+
+class ArticleNew(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    url = models.URLField(verbose_name="URL статьи", unique=True)
+
+
 
 
 
